4975e57a581d0827c2d081a0428eda807a513ad7
radoslaw-gajos <radoslawgajos@gmail.com>
2025-01-01 21:11:39 +0100

    Update authentication.rs

--- src/routes/authentication.rs
+++ src/routes/authentication.rs
@@ -1,6 +1,22 @@
 use reqwest::StatusCode;
 use crate::store::Store;
 
-pub async fn register(store: Store) -> Result<impl warp::Reply, warp::Rejection> {
-    Ok(warp::reply::with_status("Todo!", StatusCode::OK))
+pub async fn register(
+    store: Store,
+    account: Account,
+) -> Result<impl warp::Reply, warp::Rejection> {
+    //let hashed_password = hash_password(account.password.as_bytes());
+
+    let account = Account {
+        id: account.id,
+        email: account.email,
+        password: hashed_password,
+    };
+
+    match store.add_account(account).await {
+        Ok(_) => {
+            Ok(warp::reply::with_status("Account added", StatusCode::OK))
+        }
+        Err(e) => todo!(),
+    }
 }
