9d4ca4191c0568c6db0178b03471c77003e17345
radoslaw-gajos <radoslawgajos@gmail.com>
2025-01-02 20:05:38 +0100

    Fixed invalid method

--- src/routes/authentication.rs
+++ src/routes/authentication.rs
@@ -2,21 +2,7 @@ use reqwest::StatusCode;
 use crate::store::Store;
 
 pub async fn register(
-    store: Store,
-    account: Account,
+    store: Store
 ) -> Result<impl warp::Reply, warp::Rejection> {
-    //let hashed_password = hash_password(account.password.as_bytes());
-
-    let account = Account {
-        id: account.id,
-        email: account.email,
-        password: hashed_password,
-    };
-
-    match store.add_account(account).await {
-        Ok(_) => {
-            Ok(warp::reply::with_status("Account added", StatusCode::OK))
-        }
-        Err(e) => todo!(),
-    }
+    Ok(warp::reply::with_status("Todo", StatusCode::OK))
 }
