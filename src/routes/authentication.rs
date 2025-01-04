24cc6e1c326a05c7897afbc4fda5da9af211bca8
radoslaw-gajos <radoslawgajos@gmail.com>
2025-01-04 23:03:26 +0100

    Added hashing password

--- src/routes/authentication.rs
+++ src/routes/authentication.rs
@@ -1,10 +1,18 @@
 use reqwest::StatusCode;
 use crate::store::Store;
 use crate::types::account::Account;
+use argon2::Config;
+use rand::Rng;
 
 pub async fn register(
     store: Store,
     account: Account,
 ) -> Result<impl warp::Reply, warp::Rejection> {
     Ok(warp::reply::with_status("Todo", StatusCode::OK))
+}
+
+fn hash_password(password: &[u8]) -> String {
+    let salt = rand::thread_rng().gen::<[u8; 32]>();
+    let config = Config::default();
+    argon2::hash_encoded(password, &salt, &config).unwrap()
 }
