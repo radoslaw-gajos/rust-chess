4fd5d900a598c509e91885fe1ff309b8638c18b4
radoslaw-gajos <radoslawgajos@gmail.com>
2025-01-07 06:42:21 +0100

    Added checking password

--- src/routes/authentication.rs
+++ src/routes/authentication.rs
@@ -25,3 +25,10 @@ fn hash_password(password: &[u8]) -> String {
     let config = Config::default();
     argon2::hash_encoded(password, &salt, &config).unwrap()
 }
+
+fn verify_password(
+    hash: &str,
+    password: &[u8],
+) -> Result<bool, argon2::Error> {
+    argon2::verify_encoded(hash, password)
+}
