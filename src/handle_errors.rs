9b2fc81737f6bf95ff6e33d9e9c3ac97918ff6e5
radoslaw-gajos <radoslawgajos@gmail.com>
2025-01-12 18:05:08 +0100

    Handling DatabaseQueryError

--- src/handle_errors.rs
+++ src/handle_errors.rs
@@ -16,9 +16,30 @@ pub enum Error {
 impl Reject for Error {
 }
 
+const DUPLICATE_KEY: u32 = 23505;
+
 pub async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
     if let Some(Error::DatabaseQueryError(e)) = r.find() {
-        todo!();
+        match e {
+            sqlx::Error::Database(err) => {
+                if err.code().unwrap().parse::<u32>().unwrap()
+                    == DUPLICATE_KEY {
+                    Ok(warp::reply::with_status(
+                        "Entity already exists".to_string(),
+                        StatusCode::UNPROCESSABLE_ENTITY,
+                    ))
+                } else {
+                    Ok(warp::reply::with_status(
+                        "Cannot update data".to_string(),
+                        StatusCode::UNPROCESSABLE_ENTITY,
+                    ))
+                }
+            },
+            _ => Ok(warp::reply::with_status(
+                "Cannot update data".to_string(),
+                StatusCode::UNPROCESSABLE_ENTITY,
+            )),
+        }
     } else if let Some(Error::WrongPassword) = r.find() {
         Ok(warp::reply::with_status(
             "Wrong E-mail/Password combination".to_string(),
